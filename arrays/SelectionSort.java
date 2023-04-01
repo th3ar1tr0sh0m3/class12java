e7abc0a2d0b825c848045564e9f03779900452e7
themendacium <69031262+th3ar1tr0sh0m3@users.noreply.github.com>
2023-04-01 22:33:28 +0530

    Revert "Added the ascending sort method to Selection Sort program"
    This reverts commit 5fa8a8ad863549be88645a06246d6d6a87e19397.

--- arrays/SelectionSort.java
+++ arrays/SelectionSort.java
@@ -31,25 +31,6 @@ class SelectionSort {
             this.array[idx] = temp;
         }
     }
-    //core method pay attention
-    void sortDescending() {
-        for(int i = 0; i < this.size; ++i) {
-            int max = Integer.MIN_VALUE, idx = 0;
-            //the goal is to search the entire array and replace the n-th position with the n-th largest
-            //element
-            //the j loop does the searching part, the i loop maintains n and the swapping procedure
-            for(int j = i + 1; j < this.size; ++j) {
-                if(this.array[j] < max){
-                    idx = j;
-                    max = this.array[j];
-                }
-            }
-            //the swapping
-            int temp = this.array[i];
-            this.array[i] = this.array[idx];
-            this.array[idx] = temp;
-        }
-    }
     void input() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter " + this.size + " integers = ");
