64f5cad31e3ea9288756e7a86946e64e6bd813ff
themendacium <69031262+th3ar1tr0sh0m3@users.noreply.github.com>
2023-04-01 22:29:05 +0530

    Added the ascending sort method to Selection Sort program

--- arrays/SelectionSort.java
+++ arrays/SelectionSort.java
@@ -12,7 +12,25 @@ class SelectionSort {
         this.size = n;
         this.array = new int[this.size];
     }
-
+    //core method pay attention
+    void sortAscending() {
+        for(int i = 0; i < this.size; ++i) {
+            int min = Integer.MAX_VALUE, idx = 0;
+            //the goal is to search the entire array and replace the n-th position with the n-th smallest
+            //element
+            //the j loop does the searching part, the i loop maintains n and the swapping procedure
+            for(int j = i + 1; j < this.size; ++j) {
+                if(this.array[j] < min){
+                    idx = j;
+                    min = this.array[j];
+                }
+            }
+            //the swapping
+            int temp = this.array[i];
+            this.array[i] = this.array[idx];
+            this.array[idx] = temp;
+        }
+    }
     void input() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter " + this.size + " integers = ");
